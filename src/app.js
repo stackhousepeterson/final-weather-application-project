9e187cde06f65265e7f86a21db8fe87ac82ceb4b
stackhousepeterson <115060471+stackhousepeterson@users.noreply.github.com>
2022-10-21 23:00:32 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -25,11 +25,10 @@ function formatDate(timestamp) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  let forecastHTML = `<div class ="row">`;
+  let forecastHTML = "";
 
-  forecastHTML =
-    forecastHTML +
-    `<div class="col-2">
+  forecastHTML = `<div class ="row">
+    <div class="col-2">
       <div class="weather-forecast-date">Sunday</div>
       <img
         src="http://openweathermap.org/img/wn/02d@2x.png"
@@ -44,27 +43,7 @@ function displayForecast() {
   </div>
   `;
   forecastElement.innerHTML = forecastHTML;
-
-  forecastHTML =
-    forecastHTML +
-    `
-  <div class="row">
-    <div class="col-2">
-      <div class="weather-forecast-date">Sunday</div>
-      <img
-        src="http://openweathermap.org/img/wn/02d@2x.png"
-        alt=""
-        width="42"
-      />
-      <div class="weather-forecast-temperature">
-        <span class="weather-forecast-temperature-max">98℉</span>
-        <span class="weather-forecast-temperature-min">69℉</span>
-      </div>
-        </div>
-  </div>
-  `;
   forecastHTML`</div>`;
-  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
