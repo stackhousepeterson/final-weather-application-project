21251c4250e01a7d5f62f26f70558b1f95eb7329
stackhousepeterson <115060471+stackhousepeterson@users.noreply.github.com>
2022-10-21 22:46:34 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -25,7 +25,48 @@ function formatDate(timestamp) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  forecastElement.innerHTML = "Forecast";
+  let forecastHTML = `<div class ="row">`;
+
+  forecastHTML =
+    forecastHTML +
+    `
+  <div class="row">
+    <div class="col-2">
+      <div class="weather-forecast-date">Sunday</div>
+      <img
+        src="http://openweathermap.org/img/wn/02d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">98℉</span>
+        <span class="weather-forecast-temperature-min">69℉</span>
+      </div>
+    </div>
+  </div>
+  `;
+  forecastElement.innerHTML = forecastHTML;
+
+  forecastHTML =
+    forecastHTML +
+    `
+  <div class="row">
+    <div class="col-2">
+      <div class="weather-forecast-date">Sunday</div>
+      <img
+        src="http://openweathermap.org/img/wn/02d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">98℉</span>
+        <span class="weather-forecast-temperature-min">69℉</span>
+      </div>
+        </div>
+  </div>
+  `;
+  forecastHTML`</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
