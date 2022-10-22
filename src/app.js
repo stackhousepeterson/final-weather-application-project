540604cd56ba46ba33743173a102279802f7e01f
stackhousepeterson <115060471+stackhousepeterson@users.noreply.github.com>
2022-10-21 23:12:35 -0700

    moved to JS

--- src/app.js
+++ src/app.js
@@ -44,6 +44,24 @@ function displayForecast() {
   `;
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
+
+  forecastHTML =
+    forecastHTML +
+    `<div class="col-2">
+      <div class="weather-forecast-date">Sunday</div>
+      <img
+        src="http://openweathermap.org/img/wn/02d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max">98℉</span>
+        <span class="weather-forecast-temperature-min">69℉</span>
+      </div>
+    </div>
+  `;
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function displayTemperature(response) {
