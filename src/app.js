825076ccbb09bef843d058b9410388292405cdc1
stackhousepeterson <115060471+stackhousepeterson@users.noreply.github.com>
2022-10-23 01:21:01 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -40,8 +40,7 @@ function displayForecast() {
         <span class="weather-forecast-temperature-max">67°</span>
         <span class="weather-forecast-temperature-min">45°</span>
       </div>
-    </div>
-  </div>`;
+    </div>`;
   forecastHTML = `</div>`;
   forecastElement.innerHTML = forecastHTML;
 }
