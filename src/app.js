86f5334ac4f459bddcde40a5fa7b9ab4a9a1e695
stackhousepeterson <115060471+stackhousepeterson@users.noreply.github.com>
2022-10-24 21:46:29 -0700

    updated days to date

--- src/app.js
+++ src/app.js
@@ -1,3 +1,15 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+}
+
 function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
