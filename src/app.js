2d3621a3496d5557973f1f6e8fdaaa142d4b980f
stackhousepeterson <115060471+stackhousepeterson@users.noreply.github.com>
2022-10-27 22:06:10 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -12,7 +12,20 @@ function formatDate(timestamp) {
 function displayForecast() {
   let forecastElement = document.querySelector("#forecast");
 
-  forecastElement.innerHTML = "Forecast";
+  forecastElement.innerHTML = `<div class="row">
+    <div class="col-2">
+      <div clas="weather-forecast-date">Friday</div>
+      <img
+        src="http://openweathermap.org/img/wn/02d@2x.png"
+        alt=""
+        width="42"
+      />
+      <div class="weather-forecast-temperature">
+        <span class="weather-forecast-temperature-max"> 67° </span>
+        <span class="weather-forecast-temperature-min"> 45° </span>
+      </div>
+    </div>
+  </div>`;
 }
 
 function displayForecast(response) {
