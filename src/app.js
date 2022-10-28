34a2bc7072012f994e312ae2be4f496d90b3a071
stackhousepeterson <115060471+stackhousepeterson@users.noreply.github.com>
2022-10-28 00:19:05 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -26,15 +26,21 @@ function displayForecast(response) {
     forecastHTML =
       forecastHTML +
       `<div class="col-2">
-      <div clas="weather-forecast-date">${forecastDay.dt}</div>
+      <div clas="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
       <img
-        src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"; 
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"; 
         alt=""
         width="42"
       />
       <div class="weather-forecast-temperature">
-        <span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}° </span>
-        <span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}° </span>
+        <span class="weather-forecast-temperature-max"> ${
+          forecastDay.temp.max
+        }° </span>
+        <span class="weather-forecast-temperature-min"> ${
+          forecastDay.temp.min
+        }° </span>
       </div>
     </div>`;
   });
