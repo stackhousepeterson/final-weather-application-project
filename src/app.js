687d16e9b0929c8ac221f1f507dc19d37fec98d1
stackhousepeterson <115060471+stackhousepeterson@users.noreply.github.com>
2022-10-28 00:37:38 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ function displayForecast(response) {
     forecastHTML =
       forecastHTML +
       `<div class="col-2">
-      <div class = "weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      <div class = "weather-forecast-date">${formatDate(forecastDay.dt)}</div>
       <img
         src="http://openweathermap.org/img/wn/${
           forecastDay.weather[0].icon
