0e0a2a1417e67eaac359b0cf6057407dee24a46d
stackhousepeterson <115060471+stackhousepeterson@users.noreply.github.com>
2022-10-29 14:36:35 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,8 +16,6 @@ function formatDate(timestamp) {
 function displayForecast(response) {
   let forecast = response.data.daily;
 
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
-
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
