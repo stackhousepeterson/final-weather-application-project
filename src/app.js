1ca5e4878de9b712dc45b96a902c186d4dfaed98
stackhousepeterson <115060471+stackhousepeterson@users.noreply.github.com>
2022-10-29 14:52:02 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -83,9 +83,11 @@ function displayTemperature(response) {
 
   console.log({ response: response.data });
   let iconElement = document.querySelector("#icon");
-  iconElement.setAttribute("src", response.data.condition.icon_url);
-  iconElement.setAttribute("alt", response.data.condition.description);
-
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
   getForecast(response.data.coordinates);
 }
 
